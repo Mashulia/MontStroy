448354b564e71ed6dfa9c959b573cf38497e03b3
Мария <mane44ka86@mail.ru>
2019-05-22 17:32:05 +0500

    не открывает сборку

--- build/js/script.js
+++ build/js/script.js
@@ -4,9 +4,7 @@ var toggle = document.querySelector('.header__toggle');
 toggle.addEventListener('click', function() {
   if (nav.classList.contains('nav--show')) {
     nav.classList.remove('nav--show');
-    nav.classList.add('nav--hidden');
   } else {
-    nav.classList.remove('nav--hidden');
     nav.classList.add('nav--show');
   }
 });
